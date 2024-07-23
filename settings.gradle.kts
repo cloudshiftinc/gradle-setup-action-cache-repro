e49c936aebe22dc9a813f47a3028306c56c9880a
Chris Lee <chris@cloudshiftconsulting.com>
2024-07-23 15:24:54 -0700

    Updated

---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+rootProject.name = "gradle-setup-action-caching"
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("com.gradle.develocity") version "3.17.6"
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+    }
+}
+
+develocity {
+    buildScan {
+        publishing.onlyIf { System.getenv("CI") != null }
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
+    }
+}
+
---
+++ src/main/kotlin/io/cloudshifdev/repro/HelloWorldMain.kt
@@ -0,0 +1,6 @@
+package io.cloudshifdev.repro
+
+
+fun main(args: Array<String>) {
+    println("Hello, World!")
+}
