45637708297abf1f095b0b6f59c7d1bb4ca22a6e
Chris Lee <chris@cloudshiftconsulting.com>
2024-07-23 15:30:27 -0700

    Updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,16 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 
 plugins {
     kotlin("jvm") version "1.8.22"
+}
+
+tasks.withType<JavaCompile> {
+    sourceCompatibility = "17"
+    targetCompatibility = "17"
+}
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
